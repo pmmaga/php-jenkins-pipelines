17d2e975594bedf721c0c36bf6c8177c1799e768
Pedro Magalhães <mail@pmmaga.net>
2017-04-22 21:08:44 +0200

    php-src tests pipeline

---
+++ php-src-tests.groovy
@@ -0,0 +1,33 @@
+node() {
+    stage('Checkout') {
+        checkout(
+            [
+                $class: 'GitSCM', 
+                branches: [[name: BRANCH]], 
+                doGenerateSubmoduleConfigurations: false, 
+                extensions: [[$class: 'WipeWorkspace']], 
+                submoduleCfg: [], 
+                userRemoteConfigs: [[url: 'https://github.com/php/php-src.git']]
+            ]
+        );
+    }
+
+    stage('Unpack Build') {
+        def selector = [$class: 'LastCompletedBuildSelector'];
+        step ([$class: 'CopyArtifact', projectName: "php-src/${BRANCH}/build", selector: selector, parameters: "BRANCH=${BRANCH}"]);
+        def zipName = sh([script:"find -name php-*.zip", returnStdout:true]).trim();
+        fingerprint(zipName);
+        unzip([dir: 'php-install', zipFile: zipName]);
+        sh("chmod +x ./php-install/bin/php");
+        sh("chmod +x ./php-install/bin/php-cgi");
+    }
+
+    stage('Run Tests') {
+        try {
+            sh('SKIP_IO_CAPTURE_TESTS=1 NO_INTERACTION=1 TEST_PHP_JUNIT=src-junit.xml REPORT_EXIT_STATUS=1 ./php-install/bin/php run-tests.php -P -g "FAIL,XFAIL,BORK,WARN,LEAK,SKIP" --show-diff');
+        }
+        finally {
+            junit('*junit.xml');
+        }
+    }
+}
